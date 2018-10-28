558063b92b66506eeff86468fa49130daf341bdb
Eric <eric.thanenthiran@solarcentury.com>
2018-10-28 16:18:54 +0000

    created first templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,26 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
 
+    user = {'username': 'et'}
+
+    posts = [
+        {
+            'author': {'username': 'J-Dub'},
+            'body': 'Great sunday spent programming',
+            'tags': ['coding', 'flask']
+        },
+        {
+            'author': {'username': 'X-box'}, 
+            'body': 'Is car sickness genetically inherited',
+            'tags': ['life', 'worries']
+        }
+    ]
+    
+    return render_template('index.html', 
+                            title='Flask Mega', 
+                            user=user, 
+                            posts=posts)
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Flask Intro:
 
 1. Start with the Miguel Grinburg Tutorial (https://blog.miguelgrinberg.com/)
+2. [Github](https://github.com/miguelgrinberg/microblog/tree/v0.2)
