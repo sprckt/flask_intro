d4a40ae90f0797119819f682926d9c25b3d7c643
Eric <eric.thanenthiran@solarcentury.com>
2018-10-28 20:27:30 +0000

    added wtforms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -24,3 +25,11 @@ def index():
                             title='Flask Mega', 
                             user=user, 
                             posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me = {form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign in', form=form)
