11249011a7bffd21218f58cef0ff991302996a3a
Eric <eric.thanenthiran@solarcentury.com>
2018-11-13 20:54:41 +0000

    started on user logins

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
+# Initial config
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Initialise Flask extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+loging = LoginManager(app)
 
 from app import routes, models
